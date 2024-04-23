05d6c947b48a5a4f6155cbbb1d9c316ddd758375
IsaPirola <belapirola@gmail.com>
2024-04-23 16:15:50 -0300

    Set api to show selected custom object text

--- assets/controllers/app.controller.js
+++ assets/controllers/app.controller.js
@@ -2,22 +2,36 @@ app.controller("AppController", [
     "$scope",
     "zendeskService",
     function ($scope, zendeskService) {
-        // Função para buscar registro do objeto personalizado
-        $scope.getCustomObjectRecord = function () {
-            // Chamar o serviço para buscar o registro do objeto personalizado
-            zendeskService
-                .getCustomObjectRecordApi()
-                .then((response) => {
-                    // Manipular a resposta da API aqui
-                    $scope.objectText =
-                        response.custom_object_record.custom_object_fields.texto_nota;
-                })
-                .catch(function (error) {
-                    // Manipular erros aqui
-                    console.error(error);
-                });
-        };
-        // Chamada inicial para a função
-        $scope.getCustomObjectRecord();
+        var client = ZAFClient.init();
+        var objectId = "01HW5FZA3WQH7BZKCVY7ZMBDSV";
+
+        zendeskService
+            .getCustomObjectRecordApi(objectId)
+            .then((response) => {
+                console.log(response.custom_object_record.custom_object_fields.texto_nota);
+                $scope.textoNota =
+                    response.custom_object_record.custom_object_fields.texto_nota;
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
     },
+
+    // Função para buscar registro do objeto personalizado
+    // $scope.getCustomObjectRecord = function () {
+    //     // Chamar o serviço para buscar o registro do objeto personalizado
+    //     zendeskService
+    //         .getCustomObjectRecordApi()
+    //         .then((response) => {
+    //             // Manipular a resposta da API aqui
+    //             console.log(response.custom_object_record.custom_object_fields.texto_nota);
+    //             $scope.note = response.custom_object_record.custom_object_fields;
+    //         })
+    //         .catch(function (error) {
+    //             // Manipular erros aqui
+    //             console.error(error);
+    //         });
+    // };
+    // // Chamada inicial para a função
+    // $scope.getCustomObjectRecord();
 ]);
--- assets/service/zendesk.service.js
+++ assets/service/zendesk.service.js
@@ -1,15 +1,36 @@
-app.service("zendeskService", function () {
-    this.getCustomObjectRecordApi = function () {
-        const options = {
-            url: "https://flordemaio74.zendesk.com/api/v2/custom_objects/anotacao/records/01HW5FZA3WQH7BZKCVY7ZMBDSV",
-            type: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization:
-                    "Basic " + btoa("isabela.pirola@viaconsulting.com.br:Isapirol@0704"),
-            },
-        };
+app.service("zendeskService", [
+    "$q",
+    function ($q) {
+        var client = ZAFClient.init();
 
-        return client.request(options);
-    };
-});
+        this.getCustomObjectRecordApi = function (objectId) {
+            var deferred = $q.defer();
+            client
+                .request({
+                    url: `/api/v2/custom_objects/anotacao/records/${objectId}`,
+                    type: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                })
+                .then(function (response) {
+                    deferred.resolve(response);
+                })
+                .catch(function (error) {
+                    deferred.reject(error);
+                });
+
+            return deferred.promise;
+
+            // const options = {
+            //     url: "https://flordemaio74.zendesk.com/api/v2/custom_objects/anotacao/records/01HW5FZA3WQH7BZKCVY7ZMBDSV",
+            //     type: "GET",
+            //     headers: {
+            //         "Content-Type": "application/json",
+            //     },
+            // };
+
+            // return client.request(options);
+        };
+    },
+]);
