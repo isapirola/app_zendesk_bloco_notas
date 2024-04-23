d47ca736ed78151beff0a67baa249da5b2fb73f8
IsaPirola <belapirola@gmail.com>
2024-04-23 17:17:09 -0300

    Show text from existing object attributed to current user

--- assets/controllers/app.controller.js
+++ assets/controllers/app.controller.js
@@ -4,20 +4,44 @@ app.controller("AppController", [
     function ($scope, zendeskService) {
         var client = ZAFClient.init();
 
-        var userId;
-        var userName;
-        var objectId = "01HW5FZA3WQH7BZKCVY7ZMBDSV";
+        var objectId = "01HW67N3TRR2F6PZMXTVNDRY81";
 
-        zendeskService.getCurrentUser().then((response) => {
-            userName = response.currentUser.name;
-            userId = response.currentUser.id;
-        });
+        zendeskService
+            .getCurrentUser()
+            .then((response) => {
+                var userName = response.currentUser.name;
+                var userId = response.currentUser.id;
+
+                // Aqui userId estará definido, então você pode fazer a chamada para lookupCustomObjectRelationship
+                return zendeskService
+                    .lookupCustomObjectRelationship(userId)
+                    .then((response) => {
+                        if (response.count == 1) {
+                            $scope.textoNota =
+                                response.custom_object_records[0].custom_object_fields.texto_nota;
+                        } else if (response.count == 0) {
+                        } else {
+                            $scope.textoNota =
+                                "Existe mais de um objeto de notas atribuido ao usuário atual, favor pedir ajuda para a administração";
+                        }
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                        console.log(
+                            "Não foi possível obter dados da relação do objeto com o agente"
+                        );
+                    });
+            })
+            .catch(function (error) {
+                console.error(error);
+                console.log("Não foi possível obter dados do usuário atual");
+            });
 
         zendeskService
             .getCustomObjectRecordApi(objectId)
             .then((response) => {
-                $scope.textoNota =
-                    response.custom_object_record.custom_object_fields.texto_nota;
+                // $scope.textoNota =
+                //     response.custom_object_record.custom_object_fields.texto_nota;
             })
             .catch(function (error) {
                 console.error(error);
