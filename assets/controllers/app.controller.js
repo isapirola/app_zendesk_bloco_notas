a460f28d18ca5de3207c5a8398377bd192587b55
IsaPirola <belapirola@gmail.com>
2024-04-23 10:47:47 -0300

    Set the get custom object field api

--- assets/controllers/app.controller.js
+++ assets/controllers/app.controller.js
@@ -1,5 +1,23 @@
 app.controller("AppController", [
     "$scope",
     "zendeskService",
-    function ($scope, zendeskService) {},
+    function ($scope, zendeskService) {
+        // Função para buscar registro do objeto personalizado
+        $scope.getCustomObjectRecord = function () {
+            // Chamar o serviço para buscar o registro do objeto personalizado
+            zendeskService
+                .getCustomObjectRecordApi()
+                .then((response) => {
+                    // Manipular a resposta da API aqui
+                    $scope.objectText =
+                        response.custom_object_record.custom_object_fields.texto_nota;
+                })
+                .catch(function (error) {
+                    // Manipular erros aqui
+                    console.error(error);
+                });
+        };
+        // Chamada inicial para a função
+        $scope.getCustomObjectRecord();
+    },
 ]);
--- assets/service/zendesk.service.js
+++ assets/service/zendesk.service.js
@@ -1,1 +1,15 @@
-app.service("zendeskService", function ($http) {});
+app.service("zendeskService", function () {
+    this.getCustomObjectRecordApi = function () {
+        const options = {
+            url: "https://flordemaio74.zendesk.com/api/v2/custom_objects/anotacao/records/01HW5FZA3WQH7BZKCVY7ZMBDSV",
+            type: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization:
+                    "Basic " + btoa("isabela.pirola@viaconsulting.com.br:Isapirol@0704"),
+            },
+        };
+
+        return client.request(options);
+    };
+});
